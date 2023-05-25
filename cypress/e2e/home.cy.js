3bce3fd47edfae3791aebbafd52269ef5e43c296
testxico <testifyar.2099@gmail.com>
2023-05-25 08:56:34 -0500

    Adds navigation courses tests

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -44,4 +44,21 @@ describe('template spec', () => {
     cy.getByData('error-message').should('exist').contains(expectedErrorMessage);
   })
 
+  context("Courses section", () => {
+    it("Course: Testing Your First Next.js Application", () => {
+      cy.getByData("course-0").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/testing-your-first-application")
+    })
+
+    it("Course: Testing Foundations", () => {
+      cy.getByData("course-1").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/testing-foundations")
+    })
+
+    it("Course: Cypress fundamentals", () => {
+      cy.getByData("course-2").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/cypress-fundamentals")
+    })
+  })
+
 })
